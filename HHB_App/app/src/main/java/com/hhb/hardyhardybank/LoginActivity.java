3c4289c559e8f80cd2ab59cfe8f96ba3f941c56d
Hardy Lou <hlou65@gmail.com>
2014-10-31 13:33:13 -0700

    Connect Login Page to Parse
    Allows user to login to their account. Successful login
    will transfer user to MainActivity page.

--- HHB_App/app/src/main/java/com/hhb/hardyhardybank/LoginActivity.java
+++ HHB_App/app/src/main/java/com/hhb/hardyhardybank/LoginActivity.java
@@ -28,26 +28,19 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 import android.content.Intent;
+import com.parse.Parse;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.*;
+import com.parse.ParseException;
+import android.util.Log;
+import android.widget.Toast;
 
 
 /**
  * A login screen that offers login via email/password.
-
  */
 public class LoginActivity extends Activity {
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    private UserLoginTask mAuthTask = null;
-
     // UI references.
     private EditText mUsernameView;
     private EditText mPasswordView;
@@ -56,6 +49,14 @@ public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Connects app with Parse
+        Parse.initialize(this, "G9sNy6cFAc2j1ZnKVGuYKhW5gHRQdUqPV3D3BOAm", "14vOkrgINnOVIS1fSG08tdJgIsvYi7OMlw8zTFuC");
+
+        // Test Parse
+        ParseObject testObject = new ParseObject("Account");
+        testObject.put("username", "asdf");
+        testObject.saveInBackground();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
@@ -92,51 +93,66 @@ public class LoginActivity extends Activity {
      * errors are presented and no actual login attempt is made.
      */
     public void attemptLogin() {
-        if (mAuthTask != null) {
-            return;
-        }
-
         // Reset errors.
         mUsernameView.setError(null);
         mPasswordView.setError(null);
 
         // Store values at the time of the login attempt.
-        String username = mUsernameView.getText().toString();
-        String password = mPasswordView.getText().toString();
+        String input_username = mUsernameView.getText().toString();
+        String input_password = mPasswordView.getText().toString();
 
         boolean cancel = false;
         View focusView = null;
 
         // Check if username has been entered.
-        if (TextUtils.isEmpty(username)) {
+        if (TextUtils.isEmpty(input_username)) {
             mUsernameView.setError(getString(R.string.error_field_required));
             focusView = mUsernameView;
             cancel = true;
         }
 
-        if (TextUtils.isEmpty(password)) {
+        if (TextUtils.isEmpty(input_password)) {
             mPasswordView.setError(getString(R.string.error_field_required));
             focusView = mPasswordView;
             cancel = true;
         }
+
+        //TODO: check if user exists, check if password matches user
+        /*ParseQuery<ParseObject> query = ParseQuery.getQuery("Account");
+        if(query.whereContains("username", input_username) != null) {
+            cancel = false;
+        } else {
+            mUsernameView.setError(getString(R.string.error_incorrect_password));
+            focusView = mUsernameView;
+            cancel = true;
+            System.out.println("input_username = " + input_username + "\n");
+        }*/
 
         if (cancel) {
             // There was an error; don't attempt login and focus the first
             // form field with an error.
             focusView.requestFocus();
         } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            showProgress(true);
-            mAuthTask = new UserLoginTask(username, password);
-            mAuthTask.execute((Void) null);
+            ParseUser.logInInBackground(input_username, input_password, new LogInCallback() {
+                public void done(ParseUser user, ParseException e) {
+                    if (user != null) {
+                        // Successful Login, go to MainActivity
+                        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(i);
 
-            // Go to Main Account Page after successful login
-            Intent i = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(i);
+                        // Show a progress spinner, and kick off a background task to
+                        // perform the user login attempt.
+                        showProgress(true);
 
-            // Close this activity
-            finish();
+                        // Close this activity
+                        finish();
+                    } else {
+                        Toast.makeText(
+                            getApplicationContext(), "username and password do not match.",
+                                    Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
         }
     }
 
@@ -173,64 +189,6 @@ public class LoginActivity extends Activity {
             // and hide the relevant UI components.
             mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
             mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-        }
-    }
-
-
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String mUsername;
-        private final String mPassword;
-
-        UserLoginTask(String username, String password) {
-            mUsername = username;
-            mPassword = password;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mUsername)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            mAuthTask = null;
-            showProgress(false);
-
-            if (success) {
-                finish();
-            } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mAuthTask = null;
-            showProgress(false);
         }
     }
 }
