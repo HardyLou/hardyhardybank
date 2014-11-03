81416e65483b7e927226db5ea0ae9337484a583a
Hardy Lou <hlou65@gmail.com>
2014-11-03 04:19:35 -0800

    Edit CreditActivity code formatting
    Clean up comments and unnecessary code in order
    to improve readability. Add notification so that the user
    knows a deposit has been made.

--- HHB_App/app/src/main/java/com/hhb/hardyhardybank/CreditActivity.java
+++ HHB_App/app/src/main/java/com/hhb/hardyhardybank/CreditActivity.java
@@ -1,81 +1,68 @@
 package com.hhb.hardyhardybank;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Intent;
-import android.os.Build;
+
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.text.DecimalFormat;
-import com.parse.LogInCallback;
 import com.parse.Parse;
-import com.parse.ParseException;
 import com.parse.ParseObject;
-import com.parse.ParseUser;
 
 /**
- * A credit screen for users to credit their account
+ *   Credit screen for users to credit their account
  */
-
 public class CreditActivity extends Activity {
     // UI references.
     private EditText mCreditAmount;
     private TextView mDisplayBalance;
-    double credit_amount;
 
+    double balance, credit_amount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Connects app with Parse
         Parse.initialize(this, "G9sNy6cFAc2j1ZnKVGuYKhW5gHRQdUqPV3D3BOAm", "14vOkrgINnOVIS1fSG08tdJgIsvYi7OMlw8zTFuC");
 
-        // query parse for balance variable and store to display in available_balance
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credit);
 
-        // Set up the credit form.
+        // Set up the Credit Account form
         mCreditAmount = (EditText) findViewById(R.id.credit_amount);
         mDisplayBalance = (TextView) findViewById(R.id.available_balance);
 
+        // Query Parse for account balance value
         final ParseObject user_obj = new ParseObject("User");
-        double balance = user_obj.getDouble("balance");
+        balance = user_obj.getDouble("balance");
 
+        // Format displayed balance
         DecimalFormat format = new DecimalFormat("#0.00");
-        String formatted_balance = format.format(balance);
+        final String formatted_balance = format.format(balance);
         mDisplayBalance.setText("$" + formatted_balance);
-        //mDisplayBalance.setText("$" + balance);
 
-        // Activity once Sign In button is pressed
+        // Activity once Deposit button is pressed
         Button mCreditButton = (Button) findViewById(R.id.credit_enter);
         mCreditButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Adds inputted deposit amount to current balance
                 credit_amount =  Double.valueOf(mCreditAmount.getText().toString());
                 user_obj.increment("balance", credit_amount);
 
                 // TODO: Follow DRY
+                // Update the displayed balance to reflect new account balance
                 DecimalFormat format = new DecimalFormat("##.00");
                 String formatted_balance = format.format(user_obj.getDouble("balance"));
                 mDisplayBalance.setText("$" + formatted_balance);
-                //mDisplayBalance.setText("$" + user_obj.getDouble("balance"));
+
+                // Notifies user of successful deposit
+                Toast.makeText(getApplicationContext(), "Deposited $" + format.format(credit_amount)
+                               + " to credit account.",
+                        Toast.LENGTH_LONG).show();
             }
         });
-
-    }
-
-    public void creditAccount() {
-    // add input variable to current balance and store in parse
-
     }
 }
