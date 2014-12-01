40cbf23b0e848348649a967d058978e4bf07eeff
mishapps <mapang@ucsd.edu>
2014-12-01 03:37:36 -0800

    close activity for mainUser

--- HHB_App/app/src/main/java/com/hhb/hardyhardybank/MainActivityTeller.java
+++ HHB_App/app/src/main/java/com/hhb/hardyhardybank/MainActivityTeller.java
@@ -7,27 +7,16 @@ import android.widget.EditText;
 import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
-import android.widget.Toast;
-
 import com.parse.Parse;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-import com.parse.ParseUser;
-
-import java.util.List;
 
 
 /**
- *  Landing page from login; menu access to other functionality for Admins
+ * Created by dan on 11/15/14.
  */
 public class MainActivityTeller extends  ActionBarActivity{
 
     private EditText mAccountNumber;
-    private String input_account;
-    boolean cancel = false;
-    View focusView = null;
-
+    private String accountNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Connects app with Parse
@@ -36,17 +25,19 @@ public class MainActivityTeller extends  ActionBarActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_teller);
 
-        mAccountNumber = (EditText) findViewById(R.id.account_number_teller);
+
+
 
         Button mDebitButton = (Button) findViewById(R.id.action_debit);
         Button mBalanceButton = (Button) findViewById(R.id.action_balance);
-        Button mCreditButton = (Button) findViewById(R.id.action_credit);
-        Button mTransferButton = (Button) findViewById(R.id.action_transfer);
-        Button mCloseButton = (Button) findViewById(R.id.action_close);
-        Button mLogoutButton = (Button) findViewById(R.id.action_logout);
+        Button action_credit = (Button) findViewById(R.id.action_credit);
+        Button action_transfer = (Button) findViewById(R.id.action_transfer);
+        Button action_close = (Button) findViewById(R.id.action_close);
 
 
-        //TODO: SHOW BALANCE
+            // Button to credit activity
+
+        //TODO: BALANCE ACTIVITY
         // Button to Show Balance activity
         mBalanceButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,118 +51,39 @@ public class MainActivityTeller extends  ActionBarActivity{
             }
         });
 
-
-        //TODO: ACCOUNT SUMMARY
-
-
         // Button to debit activity
+
         mDebitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    checkAccount();
-                } catch (ParseException e2){
-                    Toast.makeText(getApplicationContext(), "ERROR",
-                            Toast.LENGTH_LONG).show();
-                }
-
-                if (cancel) {
-                    focusView.requestFocus();
-                }
-
-                else {
-                    // Go to Debit Activity
-                    Intent i = new Intent(MainActivityTeller.this, DebitActivity.class);
-                    i.putExtra("accountnumber", input_account);
-                    startActivity(i);
-
-                    // Close this activity
-                    finish();
-                }
-            }
-        });
-
-
-        // Button to credit activity
-        mCreditButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    checkAccount();
-                } catch (ParseException e2){
-                    Toast.makeText(getApplicationContext(), "ERROR",
-                            Toast.LENGTH_LONG).show();
-                }
-
-                if (cancel) {
-                    focusView.requestFocus();
-                }
-
-                else {
-                    // Go to Credit Activity
-                    Intent i = new Intent(MainActivityTeller.this, CreditActivity.class);
-                    i.putExtra("accountnumber", input_account);
-                    startActivity(i);
-
-                    // Close this activity
-                    finish();
-                }
-            }
-        });
-
-
-        //TODO: TRANSFER FUNDS
-
-
-        //TODO: CLOSE ACCOUNT
-
-
-        // Button to log out user
-        mLogoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ParseUser currentUser = ParseUser.getCurrentUser();
-                currentUser.logOut();
-
-                Toast.makeText(getApplicationContext(), "You have been logged out",
-                        Toast.LENGTH_LONG).show();
-
-                // Go to Login Page
-                Intent i = new Intent(MainActivityTeller.this, LoginActivity.class);
+                // Go to Debit Activity
+                Intent i = new Intent(MainActivityTeller.this, DebitActivity.class);
+                accountNumber = mAccountNumber.getText().toString();
+                i.putExtra("accountnumber", accountNumber);
                 startActivity(i);
 
                 // Close this activity
                 finish();
             }
         });
-    }
 
-    public void checkAccount() throws ParseException {
-        mAccountNumber.setError(null);
 
-        input_account = mAccountNumber.getText().toString();
+        // Button to credit activity
 
-        if (TextUtils.isEmpty(input_account)) {
-            mAccountNumber.setError(getString(R.string.error_field_required));
-            focusView = mAccountNumber;
-            cancel = true;
-        }
+        action_credit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Go to Credit Activity
+                Intent i = new Intent(MainActivityTeller.this, CreditActivity.class);
+                accountNumber = mAccountNumber.getText().toString();
+                i.putExtra("accountnumber", accountNumber);
+                startActivity(i);
 
-        else {
-            //Toast.makeText(getApplicationContext(), "ADMIN has Deposited into " + accountNumber + "'s Account!", Toast.LENGTH_LONG).show();
-            ParseQuery<ParseObject> query = ParseQuery.getQuery("Account");
-            query.whereEqualTo("accountnumber", Integer.valueOf(input_account));
-            List<ParseObject> account = query.find();
-            if (account.size() == 0) {
-                mAccountNumber.setError(getString(R.string.error_invalid_account));
-                focusView = mAccountNumber;
-                cancel = true;
+                // Close this activity
+                finish();
             }
+        });
 
-            else {
-                cancel = false;
-            }
-        }
     }
 }
 
