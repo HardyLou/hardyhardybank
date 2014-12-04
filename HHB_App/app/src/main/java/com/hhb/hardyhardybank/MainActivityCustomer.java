1af1d5befd8541a8f8d7553ab1e2228b21949f0d
Xiaohan Wang <wxhzzsf@gmail.com>
2014-12-04 02:33:05 -0800

    Finish passing data once click on one account

--- HHB_App/app/src/main/java/com/hhb/hardyhardybank/MainActivityCustomer.java
+++ HHB_App/app/src/main/java/com/hhb/hardyhardybank/MainActivityCustomer.java
@@ -1,6 +1,7 @@
 package com.hhb.hardyhardybank;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -36,8 +37,8 @@ public class MainActivityCustomer extends Activity {
 
     private String userName;
     private CustomAdapter mainActivityCustomerAdapter;
-    private TextView mAccountInfoView;
-    private TextView mBalanceView;
+    private ListView lv;
+
 
     protected void onCreate(Bundle SavedInstanceState) {
 
@@ -50,11 +51,9 @@ public class MainActivityCustomer extends Activity {
         ParseUser currentUser = ParseUser.getCurrentUser();
         userName = currentUser.getString("username");
 
-
+        ListView lv = (ListView)findViewById(R.id.listView);
         mainActivityCustomerAdapter = new CustomAdapter(this);
-
-        ListView showBalanceAndAccountType = (ListView)findViewById(R.id.listView);
-        showBalanceAndAccountType.setAdapter(mainActivityCustomerAdapter);
+        lv.setAdapter(mainActivityCustomerAdapter);
 
         // Button to Log Out Account
         Button mLogOutButton = (Button) findViewById(R.id.action_logout);
@@ -105,10 +104,14 @@ public class MainActivityCustomer extends Activity {
         });
     }
 
+
     public class CustomAdapter extends ParseQueryAdapter<ParseObject> {
 
-        private String accountType;
-        private double accountNumber;
+        Context mContext;
+        TextView mAccountInfoView;
+        TextView mBalanceView;
+        String accountType;
+        double accountNumber;
 
         public CustomAdapter(Context context) {
             // Use the QueryFactory to construct a PQA
@@ -119,21 +122,24 @@ public class MainActivityCustomer extends Activity {
                     return query;
                 }
             });
+
         }
 
         @Override
-        public View getItemView(ParseObject object, View v, ViewGroup parent) {
-            if (v == null) {
-                v = View.inflate(getContext(), R.layout.list_item, null);
+        public View getItemView(ParseObject object, View view, ViewGroup parent) {
+
+            if (view == null) {
+                view = View.inflate(getContext(),R.layout.list_item, null);
             }
 
             // Take advantage of ParseQueryAdapter's getItemView logic
             // The IDs in your custom layout must match what ParseQueryAdapter expects
-            super.getItemView(object, v, parent);
+            super.getItemView(object, view, parent);
 
             // Set up the listView item before returning the View.
-            mAccountInfoView = (TextView) v.findViewById(R.id.account_info);
-            mBalanceView = (TextView) v.findViewById(R.id.account_balance);
+            mAccountInfoView = (TextView) view.findViewById(R.id.account_info);
+            mBalanceView = (TextView) view.findViewById(R.id.account_balance);
+
 
             accountType = object.getString("accountType");
             accountNumber = object.getDouble("accountnumber");
@@ -145,7 +151,7 @@ public class MainActivityCustomer extends Activity {
             mAccountInfoView.setText("HARDY " + accountType + " (" + accountNumberFormat.format(accountNumber) + ")");
             mBalanceView.setText("$" + balanceFormat.format(balance));
 
-            v.setOnClickListener(new OnClickListener() {
+            mAccountInfoView.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View view) {
 
@@ -154,147 +160,29 @@ public class MainActivityCustomer extends Activity {
                     i.putExtra("accountnumber", accountNumber);
 
                     startActivity(i);
+
                     finish();
                 }
             });
 
-            return v;
+            mBalanceView.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Intent i = new Intent(MainActivityCustomer.this, TransactionActivity.class);
+                    i.putExtra("accountType", accountType);
+                    i.putExtra("accountnumber", accountNumber);
+
+                    startActivity(i);
+
+                    finish();
+                }
+            });
+
+            return view;
         }
     }
-/*
-    public View getNextPageView(View v, ViewGroup parent) {
-        if (v == null) {
-            // R.layout.adapter_next_page contains an ImageView with a custom graphic
-            // and a TextView.
-            v = View.inflate(getContext(), R.layout.adapter_next_page, null);
-        }
-        TextView textView = (TextView) v.findViewById(R.id.nextPageTextViewId);
-        textView.setText("Loaded " + getCount() + " rows. Get more!");
-        return v;
-    }
-*/
-
-/*
-        public class MainActivityCustomerAdapter extends BaseAdapter {
-
-            private int numOfAccounts;
-            @Override
-            public int getCount() {
-
-                // Get current user
-                ParseUser currentUser = ParseUser.getCurrentUser();
-                String userName = currentUser.getString("username");
-
-                //final int[] numOfAccounts = new int[1];
-                ParseQuery<ParseObject> query = ParseQuery.getQuery("Account");
-                query.whereEqualTo("userID", userName);
-                query.findInBackground(new FindCallback<ParseObject>() {
-                    public void done(List<ParseObject> accounts, ParseException e) {
-                        if (e == null) {
-                            //numOfAccounts[0] = accounts.size();
-                            numOfAccounts = accounts.size();
-                        } else {
-                            Log.d("score", "Error: " + e.getMessage());
-                        }
-                    }
-                });
-
-                return numOfAccounts;
-            }
-
-            @Override
-            public ParseObject getItem(final int arg0) {
-
-                ParseUser currentUser = ParseUser.getCurrentUser();
-                String userName = currentUser.getString("username");
-
-                final ParseObject[] parseObject = new ParseObject[1];
-                ParseQuery<ParseObject> query = ParseQuery.getQuery("Account");
-                query.whereEqualTo("userID", userName);
-                query.findInBackground(new FindCallback<ParseObject>() {
-                    public void done(List<ParseObject> accounts, ParseException e) {
-                        if (e == null) {
-                            parseObject[0] = accounts.get(arg0);
-                        } else {
-                            Log.d("accounts", "Error: " + e.getMessage());
-                        }
-                    }
-                });
-
-                return parseObject[0];
-            }
-
-            @Override
-            public long getItemId(int arg0) {
-
-                return arg0;
-            }
-
-            @Override
-            public View getView(final int arg0, View arg1, ViewGroup arg2) {
-
-                ParseUser currentUser = ParseUser.getCurrentUser();
-                String userName = currentUser.getString("username");
-
-                if(arg1==null)
-                {
-                    LayoutInflater inflater = (LayoutInflater) MainActivityCustomer.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                    arg1 = inflater.inflate(R.layout.activity_main_customer, arg2,false);
-                }
-
-                final TextView mAccountInfoView = (TextView)arg1.findViewById(R.id.account_info);
-                final TextView mBalanceView = (TextView)arg1.findViewById(R.id.account_balance);
-
-                ParseQuery<ParseObject> query = ParseQuery.getQuery("Account");
-                query.whereEqualTo("userID", userName);
-                query.findInBackground(new FindCallback<ParseObject>() {
-                    public void done(List<ParseObject> accounts, ParseException e) {
-                        if (e == null) {
-                            ParseObject parseObject = accounts.get(arg0);
-
-                            String accountType = parseObject.getString("accountType");
-                            double accountNumber = parseObject.getDouble("accountnumber");
-                            double balance = parseObject.getDouble("balance");
-
-                            DecimalFormat accountNumberFormat=new DecimalFormat("#.#");
-                            DecimalFormat balanceFormat = new DecimalFormat("#0.00");
-
-                            mAccountInfoView.setText("HARDY " + accountType + " (" + accountNumberFormat.format(accountNumber) + ")");
-                            mBalanceView.setText("$" + balanceFormat.format(balance));
-                        } else {
-                            Log.d("accounts", "Error: " + e.getMessage());
-                        }
-                    }
-                });
-
-                return arg1;
-
-            }
-
-        } */
-/*
-    public List<ParseObject> getDataForListView() {
-
-        // Get current user
-        ParseUser currentUser = ParseUser.getCurrentUser();
-        String userName = currentUser.getString("username");
-
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("Account");
-        query.whereEqualTo("userID", userName);
-        query.findInBackground(new FindCallback<ParseObject>() {
-            public void done(List<ParseObject> accounts, ParseException e) {
-                if (e == null) {
-                    //numOfAccounts[0] = accounts.size();
-                    numOfAccounts = accounts.size();
-                } else {
-                    Log.d("score", "Error: " + e.getMessage());
-                }
-            }
-        });
 
 
 
-    }
-
-*/
 }
--- HHB_App/app/src/main/java/com/hhb/hardyhardybank/TransactionActivity.java
+++ HHB_App/app/src/main/java/com/hhb/hardyhardybank/TransactionActivity.java
@@ -1,13 +1,17 @@
 package com.hhb.hardyhardybank;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 /**
  * Created by Xiaohan on 12/3/14.
+ * List all the transaction history for one specific account
  */
-public class TransactionActivity {
+public class TransactionActivity extends Activity {
 
     protected void onCreate(Bundle SavedInstanceState) {
+        super.onCreate(SavedInstanceState);
+        setContentView(R.layout.activity_transaction);
 
     }
 
