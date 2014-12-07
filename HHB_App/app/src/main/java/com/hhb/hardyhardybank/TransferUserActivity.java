b6249bb7f8834b37845d094b32a6506710fe2ee8
wxhzzsf <wxhzzsf@gmail.com>
2014-12-07 01:19:09 -0800

    Make sure you can't transfer to the same account and make the balance displayed in MainActivityCustomer synchronous with the transfer

--- HHB_App/app/src/main/java/com/hhb/hardyhardybank/TransferUserActivity.java
+++ HHB_App/app/src/main/java/com/hhb/hardyhardybank/TransferUserActivity.java
@@ -71,11 +71,15 @@ public class TransferUserActivity extends Activity {
             @Override
             public void onClick(View view) {
                 // stores transfer amount into variable transferAmount
-                final double transferAmount = Double.valueOf(mTransferAmount.getText().toString());
+                final double transferAmount = Integer.parseInt(mTransferAmount.getText().toString());
+
 
                 // queryCurrentUser holds the accounts of the logged in user
                 ParseQuery<ParseObject> queryCurrentUser = ParseQuery.getQuery("Account");
-                queryCurrentUser.whereEqualTo("accountnumber", mAccounts.getSelectedItem().toString());
+                ParseObject transferFrom = (ParseObject)mAccounts.getSelectedItem();
+                final int accountNumberFrom = transferFrom.getInt("accountnumber");
+
+                queryCurrentUser.whereEqualTo("accountnumber", accountNumberFrom);
                 // Get the first account found for the user
                 queryCurrentUser.getFirstInBackground(new GetCallback<ParseObject>() {
                     public void done(final ParseObject cAccountInfo, com.parse.ParseException e) {
@@ -86,50 +90,61 @@ public class TransferUserActivity extends Activity {
                             cUserBalance = cAccountInfo.getDouble("balance");
 
                             // User has sufficient funds to transfer
-                            if (cUserBalance - transferAmount >= 0) {
-                                // Subtract transfer amount from user balance
-                                cAccountInfo.increment("balance", -1 * transferAmount);
-                                cAccountInfo.saveEventually();
 
                                 // Find account associated with targetAccount
                                 ParseQuery<ParseObject> query = ParseQuery.getQuery("Account");
-                                query.whereEqualTo("accountnumber", mAccounts2.getSelectedItem().toString());
+                                ParseObject transferTo = (ParseObject)mAccounts2.getSelectedItem();
+                                final int accountNumberTo = transferTo.getInt("accountnumber");
+
+                            if (accountNumberFrom != accountNumberTo) {
+
+                                query.whereEqualTo("accountnumber", accountNumberTo);
                                 query.getFirstInBackground(new GetCallback<ParseObject>() {
                                     public void done(ParseObject targetAccount, com.parse.ParseException e) {
                                         // Target account's email is not registered in database
                                         if (targetAccount == null) {
                                             Toast.makeText(getApplicationContext(), "Target account not registered!", Toast.LENGTH_LONG).show();
                                         } else {
-                                            // Add transferred money to target account's balance
-                                            targetAccount.increment("balance", transferAmount);
-                                            targetAccount.saveEventually();
-                                            Toast.makeText(getApplicationContext(), "Success! $" +
-                                                            transferAmount + " was transferred to " +
-                                                            targetAccount.getString("userID") + ".",
-                                                    Toast.LENGTH_LONG).show();
 
-                                            // documents the transaction
-                                            // transaction log for sender
-                                            ParseObject sTransaction = new ParseObject("Transaction");
-                                            sTransaction.put("accountNumber", cAccountInfo.getInt("accountnumber"));
-                                            sTransaction.put("action", "transfer");
-                                            sTransaction.put("amount", -1 * transferAmount);
-                                            sTransaction.put("resultingBalance", cAccountInfo.getDouble("balance"));
-                                            sTransaction.saveEventually();
-                                            // transaction log for recipient
-                                            ParseObject rTransaction = new ParseObject("Transaction");
-                                            rTransaction.put("accountNumber", targetAccount.getInt("accountnumber"));
-                                            rTransaction.put("action", "transfer");
-                                            rTransaction.put("amount", transferAmount);
-                                            rTransaction.put("resultingBalance", targetAccount.getDouble("balance"));
-                                            rTransaction.saveEventually();
+
+                                            if (cUserBalance - transferAmount >= 0) {
+                                                // Subtract transfer amount from user balance
+                                                cAccountInfo.increment("balance", -1 * transferAmount);
+                                                cAccountInfo.saveEventually();
+
+                                                // Add transferred money to target account's balance
+                                                targetAccount.increment("balance", transferAmount);
+                                                targetAccount.saveEventually();
+                                                Toast.makeText(getApplicationContext(), "Success! $" +
+                                                                transferAmount + " was transferred to " +
+                                                                targetAccount.getString("userID") + ".",
+                                                        Toast.LENGTH_LONG).show();
+
+                                                // documents the transaction
+                                                // transaction log for sender
+                                                ParseObject sTransaction = new ParseObject("Transaction");
+                                                sTransaction.put("accountNumber", cAccountInfo.getInt("accountnumber"));
+                                                sTransaction.put("action", "transfer");
+                                                sTransaction.put("amount", -1 * transferAmount);
+                                                sTransaction.put("resultingBalance", cAccountInfo.getDouble("balance"));
+                                                sTransaction.saveEventually();
+                                                // transaction log for recipient
+                                                ParseObject rTransaction = new ParseObject("Transaction");
+                                                rTransaction.put("accountNumber", targetAccount.getInt("accountnumber"));
+                                                rTransaction.put("action", "transfer");
+                                                rTransaction.put("amount", transferAmount);
+                                                rTransaction.put("resultingBalance", targetAccount.getDouble("balance"));
+                                                rTransaction.saveEventually();
+                                            }
+                                            // User does not have enough money to make the transfer
+                                            else {
+                                                Toast.makeText(getApplicationContext(), "Transfer Failed! Insufficient Funds!", Toast.LENGTH_LONG).show();
+                                            }
                                         }
                                     }
                                 });
-                            }
-                            // User does not have enough money to make the transfer
-                            else {
-                                Toast.makeText(getApplicationContext(), "Transfer Failed! Insufficient Funds!", Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "You can't transfer to the same account!", Toast.LENGTH_LONG).show();
                             }
                         }
                     }
@@ -142,8 +157,9 @@ public class TransferUserActivity extends Activity {
         mReturnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Intent i = new Intent(TransferUserActivity.this, MainActivityCustomer.class);
-//                startActivity(i);
+                Intent i = new Intent(TransferUserActivity.this, MainActivityCustomer.class);
+                startActivity(i);
+
                 finish();
             }
         });
